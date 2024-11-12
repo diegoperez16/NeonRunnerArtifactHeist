69ce1f9cde43b3e7fb6a408dd65838781f4e0de6
Viviana Ramos <vivi@neonrunner.dev>
2024-11-12 09:15:00 -0500

    add audio system placeholder

---
+++ game/audio.py
@@ -0,0 +1,16 @@
+# Audio system - placeholder
+# Will use pygame.mixer once we finalize sound design
+
+class AudioManager:
+    def __init__(self):
+        # pygame.mixer.init()  -- uncomment when adding sounds
+        pass
+
+    def play_collect(self):
+        pass  # TODO: artifact pickup sound
+
+    def play_hit(self):
+        pass  # TODO: damage sound
+
+    def play_game_over(self):
+        pass  # TODO: game over jingle
